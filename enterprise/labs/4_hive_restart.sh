a7eb2e3ad76bcecb0c887cb337ce967a3f4c5736
mukkugolla007 <mukkugolla007@gmail.com>
2016-12-07 09:53:20 +0530

    Update 4_hive_restart.sh

--- enterprise/labs/4_hive_restart.sh
+++ enterprise/labs/4_hive_restart.sh
@@ -2,7 +2,7 @@
 ### Stop
 cmd1="curl  -X POST -u mukkugolla007:cloudera   \'http://localhost:7180/api/v1/clusters/mukkugolla007/services/hive/commands/stop\'"
 
-cmd="curl -X POST -u mukkugolla007:cloudera   \'http://localhost:7180/api/v1/clusters/mukkugolla007/services/hive/commands/start \'"
+cmd2="curl -X POST -u mukkugolla007:cloudera   \'http://localhost:7180/api/v1/clusters/mukkugolla007/services/hive/commands/start \'"
 echo "Soping "
 $cdm1
 echo "Starting "
